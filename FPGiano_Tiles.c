860f924079fa1c0882ac0ee9d88977a00bfd8b09
Kinkypeanut69 <balintharmse24@gmail.com>
2017-05-03 13:26:36 +0200

    added defines and logical variables
    read summary

--- FPGiano_Tiles.c
+++ FPGiano_Tiles.c
@@ -4,12 +4,22 @@
 #include "includes.h"
 #include "altera_up_avalon_character_lcd.h"
 #include "altera_up_avalon_parallel_port.h"
-#include "altera_up_avalon_video_pixel_buffer_dma.h"    // "VGA_Subsystem_VGA_Pixel_DMA"#include "altera_up_avalon_video_dma_controller.h"		// "VGA_Subsystem_Char_Buf_Subsystem_Char_Buf_DMA"#include "string.h"#include <os/alt_sem.h>
+#include "altera_up_avalon_video_pixel_buffer_dma.h"    // "VGA_Subsystem_VGA_Pixel_DMA"
+#include "altera_up_avalon_video_dma_controller.h"		// "VGA_Subsystem_Char_Buf_Subsystem_Char_Buf_DMA"
+#include "string.h"
+#include <os/alt_sem.h>
 #include "sys/alt_stdio.h"
 #include "system.h"
 #include "Altera_UP_SD_Card_Avalon_Interface.h"
 #include "altera_avalon_pio_regs.h"
-/* Definition of Task Stacks */
+
+#define	DRAW alt_up_pixel_buffer_dma_draw
+#define CLRSCREEN alt_up_pixel_buffer_dma_clear_screen(vgapixel, 0)
+#define DRAW_STRING alt_up_video_dma_draw_string
+#define TILE_WIDTH 40
+#define TILE_HEIGHT 60
+
+/* Definition of Task Stacks */
 #define   TASK_STACKSIZE       2048
 OS_STK game_stk[TASK_STACKSIZE];
 
@@ -27,47 +37,53 @@ char mode1[40] = "> Arcade <\0";
 char mode2[40] = "Disco\0";
 char mode3[40] = "Hardcore\0";
 char counter[40] = "\0";
-int nr = 0;
+int scoreCounter = 0;
 void randomKeyPress();
 int tileID;
+int white = 0xFFFF;
+int grey = 0x9CD3;
+int black = 0x1061;
+int screenHeight = 240;
+int screenWidth = 320;
 
 void randomKeyPress() {
-	int x;
-	int c1 = 0xFFFF;
-	int c2 = 0x9CD3;
+	int x;	//x-coordinate of decorative tiles
 	if (tileID > 5) {
 		x = 12 * (tileID + 15);
 	} else {
 		x = 12 * tileID;
 	}
 
-	if (tileID == 0 || tileID == 3 || tileID == 8) {
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 180, x + 7, 240, c1, 0);
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c1, 0);
-	} else if (tileID == 2 || tileID == 7 || tileID == 11) {
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x + 3, 180, x + 10, 240, c1, 0);
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c1, 0);
-	} else {
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c1, 0);
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x + 3, 180, x + 7, 240, c1, 0);
+	//color previous key white
+	if (tileID == 0 || tileID == 3 || tileID == 8) {	//tiles with black key to the right
+		DRAW_box(vgapixel, x, 0.75*screenHeight, x + 7, screenHeight, white, 0);
+		DRAW_box(vgapixel, x, 218, x + 10, screenHeight, white, 0);	//218 is used to avoid drawing over black piano keys
+	} else if (tileID == 2 || tileID == 7 || tileID == 11) {	//tiles with black key to the left
+		DRAW_box(vgapixel, x + 3, 0.75*screenHeight, x + 10, screenHeight, white, 0);
+		DRAW_box(vgapixel, x, 218, x + 10, screenHeight, white, 0);
+	} else {	//tiles with black keys on both sides
+		DRAW_box(vgapixel, x, 218, x + 10, screenHeight, white, 0);
+		DRAW_box(vgapixel, x + 3, 0.75*screenHeight, x + 7, screenHeight, white, 0);
 	}
 
-	tileID = rand() % 11;
+	tileID = rand() % 11;	//select new random key
 
+	//prevent drawing on middle of screen
 	if (tileID > 5) {
 		x = 12 * (tileID + 15);
 	} else {
 		x = 12 * tileID;
 	}
-	if (tileID == 0 || tileID == 3 || tileID == 8) {
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 180, x + 7, 240, c2, 0);
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c2, 0);
-		} else if (tileID == 2 || tileID == 7 || tileID == 11) {
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x + 3, 180, x + 10, 240, c2, 0);
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c2, 0);
-		} else {
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x, 218, x + 10, 240, c2, 0);
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x + 3, 180, x + 7, 240, c2, 0);
+	//color current key grey
+	if (tileID == 0 || tileID == 3 || tileID == 8) {	//tiles with black key to the right
+			DRAW_box(vgapixel, x, 0.75*screenHeight, x + 7, screenHeight, grey, 0);
+			DRAW_box(vgapixel, x, 218, x + 10, screenHeight, grey, 0);
+		} else if (tileID == 2 || tileID == 7 || tileID == 11) {	//tiles with black key to the left
+			DRAW_box(vgapixel, x + 3, 0.75*screenHeight, x + 10, screenHeight, grey, 0);
+			DRAW_box(vgapixel, x, 218, x + 10, screenHeight, grey, 0);
+		} else {	//tiles with black keys on both sides
+			DRAW_box(vgapixel, x, 218, x + 10, screenHeight, grey, 0);
+			DRAW_box(vgapixel, x + 3, 0.75*screenHeight, x + 7, screenHeight, grey, 0);
 		}
 }
 
@@ -102,133 +118,121 @@ void VGA_box(int x1, int y1, int x2, int y2, int pixel_color){
 }
 
 void game(void* pdata) {
-	int x1 = 80, y1 = 0, x2 = 240, y2 = 240;
-	int x3 = 120, y3 = 0, x4 = 120, y4 = 240;
-	int x5 = 80, y5 = 0, x6 = 120;
-	int x7 = 160, x8 = 200, x9 = 160;
-	int y6 = 60, y7 = 120, y8 = 180;
-	int y52 = 60, y62 = 120;
-	int y72 = 180, y82 = 240;
-	int y9 = 240, y92 = 300;
-	int i, j;
-	int c1 = 0xFFFF;
-	int c2 = 0x9CD3;
-	int c3 = 0x1061;
-
+	int decorativeWhiteTile, decorativeBlackTile;
+	int screenTop = 0;
+	int block1_x_left = screenWidth*0.25, 					block1_x_right = block1_x_left + TILE_WIDTH, 	block1_y_top = screenTop, 					block1_y_bottom = screenTop + TILE_HEIGHT;
+	int block2_x_left = screenWidth*0.25 + TILE_WIDTH, 		block2_x_right = block2_x_left + TILE_WIDTH, 	block2_y_top = screenTop + TILE_HEIGHT, 	block2_y_bottom = screenTop + TILE_HEIGHT*2;
+	int block3_x_left = screenWidth*0.25 + TILE_WIDTH*2, 	block3_x_right = block3_x_left + TILE_WIDTH, 	block3_y_top = screenTop + TILE_HEIGHT*2, 	block3_y_bottom = screenTop + TILE_HEIGHT*3;
+	int block4_x_left = screenWidth*0.25 + TILE_WIDTH*3, 	block4_x_right = block4_x_left + TILE_WIDTH, 	block4_y_top = screenTop + TILE_HEIGHT*3, 	block4_y_bottom = screenTop + TILE_HEIGHT*4;
+	int block5_x_left = screenWidth*0.25 + TILE_WIDTH*4, 	block5_x_right = block5_x_left + TILE_WIDTH, 	block5_y_top = screenTop + TILE_HEIGHT*4, 	block5_y_bottom = screenTop + TILE_HEIGHT*5;
 	int blockSelect;
 
 	while (1) {
-		for (i = 0; i < 27; i++) {
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, i * 12, 180,
-					(i * 12) + 10, 240, c1, 0);
+		for (decorativeWhiteTile = 0; decorativeWhiteTile < 27; decorativeWhiteTile++) {	//draws 27 white piano keys on background
+			DRAW_box(vgapixel, decorativeWhiteTile * 12, screenHeight*0.75,
+					(decorativeWhiteTile * 12) + 10, screenHeight, white, 0);
 		}
-		for (j = 0; j < 26; j++) {
-			if (j == 2 || j == 6 || j == 19 || j == 22) {
+		for (decorativeBlackTile = 0; decorativeBlackTile < 26; decorativeBlackTile++) {	//draws 26 black piano keys on background
+			if (decorativeBlackTile == 2 || decorativeBlackTile == 6 || decorativeBlackTile == 19 || decorativeBlackTile == 22) {
 			} else {
-				alt_up_pixel_buffer_dma_draw_box(vgapixel, j * 12 + 8, 180,
-						(j * 12) + 14, 217, c3, 0);
+				DRAW_box(vgapixel, decorativeBlackTile * 12 + 8, screenHeight*0.75,
+						(decorativeBlackTile * 12) + 14, 217, black, 0);
 			}
 		}
 
-		alt_up_pixel_buffer_dma_draw_box(vgapixel, x1, y1, x2, y2, c1, 0);
+		DRAW_box(vgapixel, screenWidth*0.25, screenTop, screenWidth*0.75, screenHeight, white, 0); //white game box
 
-		alt_up_pixel_buffer_dma_draw_line(vgapixel, x3, y3, x4, y4, c2, 0);
-		alt_up_pixel_buffer_dma_draw_line(vgapixel, x3 + 40, y3, x4 + 40, y4,
-				c2, 0);
-		alt_up_pixel_buffer_dma_draw_line(vgapixel, x3 + 80, y3, x4 + 80, y4,
-				c2, 0);
+		DRAW_line(vgapixel, screenWidth*0.375, screenTop, screenWidth*0.375, screenHeight, grey, 0);
+		DRAW_line(vgapixel, screenWidth*0.5, screenTop, screenWidth*0.5, screenHeight, grey, 0);
+		DRAW_line(vgapixel, screenWidth*0.625, screenTop, screenWidth*0.625, screenHeight, grey, 0);
 
 		while (1) {
 
-			if (y52 - y5 == 60) {
-				y5++;
-				alt_up_pixel_buffer_dma_draw_line(vgapixel, x5 + 1, y5 - 1,
-						x5 + 39, y5 - 1, c1, 0);
+			if (block1_y_bottom - block1_y_top == TILE_HEIGHT) {
+				block1_y_top++;
+				DRAW_line(vgapixel, block1_x_left + 1, block1_y_top - 1, block1_x_right - 1, block1_y_top - 1, white, 0);
 			}
-			y52++;
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x5 + 1, y5, x5 + 39, y52,
-					c3, 0);
+			block1_y_bottom++;
+			DRAW_box(vgapixel, block1_x_left + 1, block1_y_top, block1_x_right - 1, block1_y_bottom, black, 0);
 
-			if (y62 - y6 == 60) {
-				y6++;
-				alt_up_pixel_buffer_dma_draw_line(vgapixel, x6 + 1, y6 - 1,
-						x6 + 39, y6 - 1, c1, 0);
+			if (block2_y_bottom - block2_y_top == TILE_HEIGHT) {
+				block2_y_top++;
+				DRAW_line(vgapixel, block2_x_left + 1, block2_y_top - 1, block2_x_right - 1, block2_y_top - 1, white, 0);
 			}
-			y62++;
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x6 + 1, y6, x6 + 39, y62,
-					c3, 0);
+			block2_y_bottom++;
+			DRAW_box(vgapixel, block2_x_left + 1, block2_y_top, block2_x_right - 1, block2_y_bottom, black, 0);
 
-			if (y72 - y7 == 60) {
-				y7++;
-				alt_up_pixel_buffer_dma_draw_line(vgapixel, x7 + 1, y7 - 1,
-						x7 + 39, y7 - 1, c1, 0);
+			if (block3_y_bottom - block3_y_top == TILE_HEIGHT) {
+				block3_y_top++;
+				DRAW_line(vgapixel, block3_x_left + 1, block3_y_top - 1, block3_x_right - 1, block3_y_top - 1, white, 0);
 			}
-			y72++;
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x7 + 1, y7, x7 + 39, y72,
-					c3, 0);
+			block3_y_bottom++;
+			DRAW_box(vgapixel, block3_x_left + 1, block3_y_top, block3_x_right - 1, block3_y_bottom, black, 0);
 
-			if (y82 - y8 == 60) {
-				y8++;
-				alt_up_pixel_buffer_dma_draw_line(vgapixel, x8 + 1, y8 - 1,
-						x8 + 39, y8 - 1, c1, 0);
+			if (block4_y_bottom - block4_y_top == TILE_HEIGHT) {
+				block4_y_top++;
+				DRAW_line(vgapixel, block4_x_left + 1, block4_y_top - 1, block4_x_right - 1, block4_y_top - 1, white, 0);
 			}
-			y82++;
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x8 + 1, y8, x8 + 39, y82,
-					c3, 0);
+			block4_y_bottom++;
+			DRAW_box(vgapixel, block4_x_left + 1, block4_y_top, block4_x_right - 1, block4_y_bottom, black, 0);
 
-			if (y92 - y9 == 60) {
-				y9++;
-				alt_up_pixel_buffer_dma_draw_line(vgapixel, x9 + 1, y9 - 1,
-						x9 + 39, y9 - 1, c1, 0);
+			if (block5_y_bottom - block5_y_top == TILE_HEIGHT) {
+				block5_y_top++;
+				DRAW_line(vgapixel, block5_x_left + 1, block5_y_top - 1, block5_x_right - 1, block5_y_top - 1, white, 0);
 			}
-			y92++;
-			alt_up_pixel_buffer_dma_draw_box(vgapixel, x9 + 1, y9, x9 + 39, y92,
-					c3, 0);
+			block5_y_bottom++;
+			DRAW_box(vgapixel, block5_x_left + 1, block5_y_top, block5_x_right - 1, block5_y_bottom, black, 0);
 
-			if (y5 > 240) {
+
+
+			if (block1_y_top > screenHeight) {
 				blockSelect = rand() % 4;
-				x5 = (blockSelect * 40) + 80;
-				y5 = 0;
-				y52 = 0;
+				block1_x_left = (blockSelect * TILE_WIDTH) + screenWidth*0.25;
+				block1_y_top = 0;
+				block1_y_bottom = 0;
 				randomKeyPress();
-				nr++;
+				scoreCounter++;
 			}
 
-			if (y6 > 240) {
+			if (block2_y_top > screenHeight) {
 				blockSelect = rand() % 4;
-				x6 = (blockSelect * 40) + 80;
-				y6 = 0;
-				y62 = 0;
+				block2_x_left = (blockSelect * TILE_WIDTH) + screenWidth*0.25;
+				block2_y_top = 0;
+				block2_y_bottom = 0;
 				randomKeyPress();
-				nr++;
+				scoreCounter++;
 			}
-			if (y7 > 240) {
+
+			if (block3_y_top > screenHeight) {
 				blockSelect = rand() % 4;
-				x7 = (blockSelect * 40) + 80;
-				y7 = 0;
-				y72 = 0;
+				block3_x_left = (blockSelect * TILE_WIDTH) + screenWidth*0.25;
+				block3_y_top = 0;
+				block3_y_bottom = 0;
 				randomKeyPress();
-				nr++;
+				scoreCounter++;
 			}
-			if (y8 > 240) {
+
+			if (block4_y_top > screenHeight) {
 				blockSelect = rand() % 4;
-				x8 = (blockSelect * 40) + 80;
-				y8 = 0;
-				y82 = 0;
+				block4_x_left = (blockSelect * TILE_WIDTH) + screenWidth*0.25;
+				block4_y_top = 0;
+				block4_y_bottom = 0;
 				randomKeyPress();
-				nr++;
+				scoreCounter++;
 			}
-			if (y9 > 240) {
+
+			if (block5_y_top > screenHeight) {
 				blockSelect = rand() % 4;
-				x9 = (blockSelect * 40) + 80;
-				y9 = 0;
-				y92 = 0;
+				block5_x_left = (blockSelect * TILE_WIDTH) + screenWidth*0.25;
+				block5_y_top = 0;
+				block5_y_bottom = 0;
 				randomKeyPress();
-				nr++;
+				scoreCounter++;
 			}
+
 			OSTimeDlyHMSM(0, 0, 0, 60);
-			sprintf(counter, "%d", nr);
-			alt_up_video_dma_draw_string(vgachar, counter, 67, 7, 0);
+			sprintf(counter, "%d", scoreCounter);
+			DRAW_STRING(vgachar, counter, 67, 7, 0);
 		}
 	}
 }
@@ -247,7 +251,7 @@ int main(void) {
 	} else
 		printf("Opened VGA_Pixel_Buffer device\n");
 
-	alt_up_pixel_buffer_dma_clear_screen(vgapixel, 0);			//clear screen
+	CLRSCREEN;			//clear screen
 
 	vgachar = alt_up_video_dma_open_dev(
 			"/dev/VGA_Subsystem_Char_Buf_Subsystem_Char_Buf_DMA");//open char buffer
@@ -259,11 +263,11 @@ int main(void) {
 
 	alt_up_video_dma_screen_clear(vgachar, 0);					//clear buffer
 
-	alt_up_video_dma_draw_string(vgachar, text_top_row, 1, 2, 0);
-	alt_up_video_dma_draw_string(vgachar, text_bottom_row, 61, 2, 0);
-	alt_up_video_dma_draw_string(vgachar, mode1, 3, 8, 0);
-	alt_up_video_dma_draw_string(vgachar, mode2, 3, 12, 0);
-	alt_up_video_dma_draw_string(vgachar, mode3, 3, 16, 0);
+	DRAW_STRING(vgachar, text_top_row, 1, 2, 0);
+	DRAW_STRING(vgachar, text_bottom_row, 61, 2, 0);
+	DRAW_STRING(vgachar, mode1, 3, 8, 0);
+	DRAW_STRING(vgachar, mode2, 3, 12, 0);
+	DRAW_STRING(vgachar, mode3, 3, 16, 0);
 
 	OSTaskCreateExt(game, NULL, (void *) &game_stk[TASK_STACKSIZE - 1],
 	game_PRIORITY, game_PRIORITY, game_stk, TASK_STACKSIZE, NULL, 0);
